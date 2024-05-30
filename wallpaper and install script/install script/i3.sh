ac9abeac5e355743df606f297d3e98682845bb4b
AyakoGFX <ayako.gfx@gmail.com>
2024-05-30 22:51:24 +0530

    added wall and install script

---
+++ wallpaper and install script/install script/i3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install i3ipc using paru
+paru i3ipc
+
+# Choose glib2
+# Note: You might need to manually choose glib2 here depending on your paru configuration.
+# If prompted, select the version you prefer.
+paru i3-workspaces
+
+yay -S i3-auto-layout
+
+# Install ttf-firacode-nerd using pacman
+sudo pacman -S ttf-firacode-nerd
+
+# Install nitrogen using pacman
+sudo pacman -S nitrogen
+
+# Install i3 window manager using pacman
+sudo pacman -S i3
+
+sudo pacman -S alacritty
+
+git clone https://github.com/marcoplaitano/wallpapers.git ~/Pictures
+
---
+++ wallpaper and install script/install script/pi.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Update system
+sudo pacman -Syu --noconfirm
+
+# Install essential packages
+sudo pacman -S --noconfirm base-devel
+sudo pacman -S --noconfirm git
+sudo pacman -S --noconfirm python
+sudo pacman -S --noconfirm nodejs
+sudo pacman -S --noconfirm yay
+sudo pacman -S --noconfirm cmake
+sudo pacman -S --noconfirm npm
+sudo pacman -S --noconfirm lua51
+sudo pacman -S --noconfirm luajit
+sudo pacman -S --noconfirm gcc
+sudo pacman -S --noconfirm luarocks
+sudo pacman -S --noconfirm neovim
+sudo luarocks install luarocks.nvim
+sudo pacman -S --noconfirm emacs
+sudo pacman -S --noconfirm google-chrome
+sudo pacman -S --noconfirm fzf
+sudo pacman -S --noconfirm zsh
+sudo pacman -S --noconfirm xclip
+yay -S --noconfirm paru
+yay -S --noconfirm paru-bin
+sudo pacman -S gnome-disk-utility
+sudo pacman -S baobab
+sudo pacman -S ripgrep
+sudo pacman -S ttf-firacode-nerd
+sudo pacman -S krusader
+# yay -S Kmonad-bin
+#sudo pacman -S xorg-xmodmap
+#sudo pacman -S xorg-xev
+#sudo pacman -S xorg-setxkbmap
+#sudo pacman -S xorg-xset
+#Identifying Keys Script:
+# xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'
+# xmodmap -e 'keycode 66 = Escape'
+# xmodmap -e 'keycode 108 = Caps_Lock'
+# xmodmap -e 'clear lock'
+#
+#
+#
+#
+#
+# Install input-remapper-git
+#yay -S input-remapper-git
+
+# Restart input-remapper service
+#sudo systemctl restart input-remapper
+
+# Enable input-remapper service to start on boot
+#sudo systemctl enable input-remapper
+
+#echo "input-remapper-git installed and configured."
