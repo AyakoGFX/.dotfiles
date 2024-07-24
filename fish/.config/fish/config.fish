096238275163b7a5780a20fc1dcaa73e3cfdc634
ayako <ayako.gfx@gmail.com>
2024-07-24 21:43:45 +0530

    z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -148,6 +148,9 @@ set -x PATH $HOME/.config/emacs/bin $PATH
 # Initialize zoxide in Fish shell
 zoxide init fish | source
 
+set -x EDITOR nvim
+set -x VISUAL nvim
+
 set -x KITTY_FONT_FAMILY "FiraCode Nerd Font"
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
