3f645e05b219fd48802e0642c643b727764dea76
ayako <ayako.gfx@gmail.com>
2024-07-31 22:10:40 +0530

    z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -142,6 +142,8 @@ alias remove='sudo pacman -R'
 alias speed='xset r rate 250 60'
 alias tldrf='tldr --list | fzf --preview "tldr {1}" --preview-window=right,60% | xargs tldr'
 alias e='emacsclient -c &'
+
+alias smci='sudo make clean install && rm *.o && rm *.orig'
 # Path modification
 set -x PATH $HOME/.config/emacs/bin $PATH
 
