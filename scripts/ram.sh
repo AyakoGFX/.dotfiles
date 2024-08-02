d662c270a7e1c9d78ccccbe69c538b2c0e2910e4
ayako <ayako.gfx@gmail.com>
2024-08-02 22:04:06 +0530

    z

---
+++ scripts/ram.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+sudo uptime | lolcat
+sudo free -m | lolcat
+
+sudo sync; echo 1 > /proc/sys/vm/drop_caches
+sudo sync; echo 2 > /proc/sys/vm/drop_caches
+sudo sync; echo 3 > /proc/sys/vm/drop_caches 
+
+
+
+sudo swapoff -a
+sudo swapon -a
+
+fish
