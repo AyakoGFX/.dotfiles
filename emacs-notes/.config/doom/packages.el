ce9c7a99f771fd0dbf6794eff62a8a47195ac67b
ayako <ayako.gfx@gmail.com>
2024-09-16 22:21:23 +0530

    doom eamcs add es dashbord

--- emacs-notes/.config/doom/packages.el
+++ emacs-notes/.config/doom/packages.el
@@ -48,7 +48,7 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
-
+(package! dashboard)
 (package! org-modern)
 (package! org-superstar)
 (package! olivetti)
