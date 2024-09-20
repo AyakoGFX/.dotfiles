1531a232640b7b61f73da0eda722feb9a55c19fc
ayako <ayako.gfx@gmail.com>
2024-09-20 23:23:52 +0530

    z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -311,3 +311,10 @@ pokemon-colorscripts -r
 # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 # " 
 
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
