005db698ff6f0aa901915ff14a7ac371ebd1366c
ayako <ayako.gfx@gmail.com>
2024-09-23 19:43:50 +0530

    amd intal nvidia added .sh

--- scripts/amd-nvidia-setup/amd.sh
+++ scripts/amd-nvidia-setup/amd.sh
@@ -1,6 +1,8 @@
-sudo pacman -S --noconfirm --needed amd-ucode
-sudo pacman -S --noconfirm --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils
+# sudo pacman -S --noconfirm --needed amd-ucode
+# sudo pacman -S --noconfirm --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils
 
+sudo pacman -S --needed --noconfirm mesa xf86-video-amdgpu amdvlk lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers lib32-vulkan-mesa-layers libva-mesa-driver lib32-libva-mesa-driver
+sudo pacman -S --needed --noconfirm mesa-vdpau lib32-mesa-vdpau rocm-opencl-runtime rocm-hip-runtime
 # yay -S lact
 # https://github.com/ilya-zlobintsev/LACT.git
 # https://youtu.be/um0PibK4Mtk?si=Gcq6GnkqR_rQUOCZ
--- scripts/amd-nvidia-setup/nvidia.sh
+++ scripts/amd-nvidia-setup/nvidia.sh
@@ -5,5 +5,15 @@
 
 # pacman -S --noconfirm nvidia
  
- 
-echo "open me" 
+ # https://github.com/xerolinux/xlapit-cli/blob/main/xero-scripts/scripts/drivers.sh
+# echo "open me" 
+
+read -rp "Older 900/1000 series or Newer 1650ti/1660ti/20 series and up? (o/n): " nvidia_series
+if [[ $nvidia_series == "o" || $nvidia_series == "1000" ]]; then
+  sudo pacman -S --needed --noconfirm linux-headers nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland opencl-nvidia lib32-opencl-nvidia libvdpau-va-gl libvdpau
+elif [[ $nvidia_series == "n" ]]; then
+  sudo pacman -S --needed --noconfirm linux-headers nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland opencl-nvidia lib32-opencl-nvidia libvdpau-va-gl libvdpau
+else
+  echo "Invalid selection."
+  return
+fi
