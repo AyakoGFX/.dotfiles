9dee94f0a05937cc13806163cfe41992e649334d
ayako <ayako.gfx@gmail.com>
2024-10-04 19:46:08 +0530

    junk files

---
+++ gnu-emacs/.emacs.d/elpa/vundo-2.3.0/vundo-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from vundo.el  -*- no-byte-compile: t -*-
+(define-package "vundo" "2.3.0" "Visual undo tree" '((emacs "28.1")) :commit "4c01db9c011b7b4ca4ef5da338cdc6ea45aac7a5" :authors '(("Yuan Fu" . "casouri@gmail.com")) :maintainer '("Yuan Fu" . "casouri@gmail.com") :keywords '("undo" "text" "editing") :url "https://github.com/casouri/vundo")
