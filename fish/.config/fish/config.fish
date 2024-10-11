0253b4c9483d68932a8baadbc8bbe8c14920d588
ayako <ayako.gfx@gmail.com>
2024-10-11 17:00:43 +0530

    z

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,94 +3,94 @@ set TERM "xterm-256color"                         # Sets the terminal type
 
 # name: sashimi
 ##################################################################################################################
-function fish_prompt
-    set -l user (whoami)
-    set -l my_hostname (hostnamectl --static 2>/dev/null || uname -n)
-    set -l pwd (pwd)
+# function fish_prompt
+#     set -l user (whoami)
+#     set -l my_hostname (hostnamectl --static 2>/dev/null || uname -n)
+#     set -l pwd (pwd)
 
-    # Use ~ for the home directory specifically
-    if test "$pwd" = "$HOME"
-        set pwd "~/"
-    else if string match -r "^$HOME/" $pwd
-        set pwd (string replace -r "^$HOME" "~" $pwd)
-    end
+#     # Use ~ for the home directory specifically
+#     if test "$pwd" = "$HOME"
+#         set pwd "~/"
+#     else if string match -r "^$HOME/" $pwd
+#         set pwd (string replace -r "^$HOME" "~" $pwd)
+#     end
 
-    printf "\033[1;31m[%s\033[1;33m@%s \033[1;32m%s\033[1;35m] \033[1;31m\033[0m" $user $my_hostname $pwd
-end
+#     printf "\033[1;31m[%s\033[1;33m@%s \033[1;32m%s\033[1;35m] \033[1;31m\033[0m" $user $my_hostname $pwd
+# end
 ####################################################################################################################
 # # name: sashimi
-# function fish_prompt
-#   set -l last_status $status
-#   set -l cyan (set_color -o cyan)
-#   set -l yellow (set_color -o yellow)
-#   set -g red (set_color -o red)
-#   set -g blue (set_color -o blue)
-#   set -l green (set_color -o green)
-#   set -g normal (set_color normal)
+function fish_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -g red (set_color -o red)
+  set -g blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -g normal (set_color normal)
 
-#   set -l ahead (_git_ahead)
-#   set -g whitespace ' '
+  set -l ahead (_git_ahead)
+  set -g whitespace ' '
 
-#   if test $last_status = 0
-#     set initial_indicator "$green◆"
-#     set status_indicator "$normal❯$cyan❯$green❯"
-#   else
-#     set initial_indicator "$red✖ $last_status"
-#     set status_indicator "$red❯$red❯$red❯"
-#   end
-#   set -l cwd $cyan(basename (prompt_pwd))
+  if test $last_status = 0
+    set initial_indicator "$green◆"
+    set status_indicator "$normal❯$cyan❯$green❯"
+  else
+    set initial_indicator "$red✖ $last_status"
+    set status_indicator "$red❯$red❯$red❯"
+  end
+  set -l cwd $cyan(basename (prompt_pwd))
 
-#   if [ (_git_branch_name) ]
+  if [ (_git_branch_name) ]
 
-#     if test (_git_branch_name) = 'master'
-#       set -l git_branch (_git_branch_name)
-#       set git_info "$normal git:($red$git_branch$normal)"
-#     else
-#       set -l git_branch (_git_branch_name)
-#       set git_info "$normal git:($blue$git_branch$normal)"
-#     end
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($blue$git_branch$normal)"
+    end
 
-#     if [ (_is_git_dirty) ]
-#       set -l dirty "$yellow ✗"
-#       set git_info "$git_info$dirty"
-#     end
-#   end
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+    end
+  end
 
-#   # Notify if a command took more than 5 minutes
-#   if [ "$CMD_DURATION" -gt 300000 ]
-#     echo The last command took (math "$CMD_DURATION/1000") seconds.
-#   end
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
 
-#   echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
-# end
+  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
+end
 
-# function _git_ahead
-#   set -l commits (command git rev-list --left-right '@{upstream}...HEAD' 2>/dev/null)
-#   if [ $status != 0 ]
-#     return
-#   end
-#   set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
-#   set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
-#   switch "$ahead $behind"
-#     case ''     # no upstream
-#     case '0 0'  # equal to upstream
-#       return
-#     case '* 0'  # ahead of upstream
-#       echo "$blue↑$normal_c$ahead$whitespace"
-#     case '0 *'  # behind upstream
-#       echo "$red↓$normal_c$behind$whitespace"
-#     case '*'    # diverged from upstream
-#       echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
-#   end
-# end
+function _git_ahead
+  set -l commits (command git rev-list --left-right '@{upstream}...HEAD' 2>/dev/null)
+  if [ $status != 0 ]
+    return
+  end
+  set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
+  set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
+  switch "$ahead $behind"
+    case ''     # no upstream
+    case '0 0'  # equal to upstream
+      return
+    case '* 0'  # ahead of upstream
+      echo "$blue↑$normal_c$ahead$whitespace"
+    case '0 *'  # behind upstream
+      echo "$red↓$normal_c$behind$whitespace"
+    case '*'    # diverged from upstream
+      echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
+  end
+end
 
-# function _git_branch_name
-#   echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
-# end
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
+end
 
-# function _is_git_dirty
-#   echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
-# end
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty 2>/dev/null)
+end
 
 ########################################################################################
 
