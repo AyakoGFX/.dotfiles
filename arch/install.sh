ca558c4996191df49f5d97fdb1c78d1ed16bffc9
ayako <ayako.gfx@gmail.com>
2025-02-23 21:27:58 +0530

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -1,93 +1,104 @@
-xdg-user-dirs-update
+#!/bin/bash
+
+# Update the system
+sudo pacman -Syu
+
+# Essential Packages
+sudo pacman -S --noconfirm \
+    emacs \
+    git \
+    pkgconf \
+    cmake \
+    fd
+
+# Text Processing
+sudo pacman -S --noconfirm \
+    hunspell hunspell-en_us \
+    enchant
+
+# Development Tools
+sudo pacman -S --noconfirm \
+    base-devel \
+    fish \
+    tldr \
+    stow
+
+# System Utilities
+sudo pacman -S --noconfirm \
+    acpi \
+    brightnessctl \
+    ripgrep \
+    picom \
+    gdu \
+    termdown
+
+# Desktop Environment
+sudo pacman -S --noconfirm \
+    awesome \
+    alacritty \
+    sxhkd \
+    flameshot \
+    polkit-gnome \
+    eog \
+    nitrogen \
+    copyq \
+    vlc \
+    ffmpeg \
+    pavucontrol \
+    yt-dlp \
+    file-roller
+
+sudo pacman -S --noconfirm \
+    lightdm \
+    lightdm-gtk-greeter
+    
+sudo systemctl enable lightdm
+sudo systemctl start lightdm
 
 
+# Utilities
+sudo pacman -S --noconfirm \
+    zoxide \
+    eza \
+    trash-cli \
+    ugrep \
+    neofetch \
+    fastfetch \
+    openrgb \
+    xfce4-taskmanager \
+    gvfs \
+    xdotool \
+    xorg-xwininfo \
+    network-manager-applet
 
-# Emacs Dependency
-emacs
-hunspell
-hunspell-en_us
-enchant
-pkgconf
-cmake
-fd
-git
+# Appearance and Themes
+sudo pacman -S --noconfirm \
+    lxappearance \
+    kvantum \
+    qt5ct \
+    arc-gtk-theme \
+    papirus-icon-theme
 
-# Desktop
+# Audio Packages
+sudo pacman -S --noconfirm \
+    pipewire-pulse \
+    pipewire-alsa
 
+# Fonts
+sudo pacman -S --noconfirm \
+    ttf-firacode-nerd \
+    ttf-jetbrains-mono-nerd \
+    noto-fonts \
+    emoji-font \
+    ttf-font \
+    ttf-ms-fonts \
+    ttf-liberation \
+    terminus-font
 
-base-devel
-fish
-tldr
-stow
-acpi
-brightnessctl
-ripgrep
-picom
-gdu
-termdown
+# AUR Packages
+sudo yay -S \
+    bitwarden-bin \
+    github-desktop \
+    normcap
 
-awesome
-alacritty
-sxhkd
-flameshot
-polkit-gnome
-eog
-nitrogen
-copyq
-vlc
-ffmpeg
-pavucontrol
-yt-dlp
-file-roller
-
-zoxide
-eza
-trash-cli
-ugrep
-neofetch
-fastfetch
-openrgb
-xfce4-taskmanager
-gvfs
-xdotool
-xorg-xwininfo
-network-manager-applet
-
-lxappearance
-kvantum
-qt5ct
-arc-gtk-theme
-papirus-icon-theme
-
-# audio
-pipewire-pulse
-pipewire-alsa
-
-ttf-firacode-nerd
-ttf-jetbrains-mono-nerd
-noto-fonts
-emoji-font
-ttf-font
-ttf-ms-fonts
-ttf-liberation
-terminus-font
-
-# AUR
-bitwarden-bin
-github-desktop
-normcap
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "Installation complete!"
