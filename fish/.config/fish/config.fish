cf41db6024185877872948931afc7975de618e31
Ayako <ayako.gfx@gmail.com>
2025-03-02 03:25:08 +0530

    trans-toggle

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -220,43 +220,7 @@ Alt + .     Use the last word of the previous command
 end
 # PATH
 set -gx PATH $HOME/.local/bin $PATH
-
-# SPECIAL FUNCTIONS
-# Extracts any archive(s)
-function extract
-    for archive in $argv
-        if test -f $archive
-            switch $archive
-                case *.tar.bz2
-                    tar xvjf $archive
-                case *.tar.gz
-                    tar xvzf $archive
-                case *.bz2
-                    bunzip2 $archive
-                case *.rar
-                    rar x $archive
-                case *.gz
-                    gunzip $archive
-                case *.tar
-                    tar xvf $archive
-                case *.tbz2
-                    tar xvjf $archive
-                case *.tgz
-                    tar xvzf $archive
-                case *.zip
-                    unzip $archive
-                case *.Z
-                    uncompress $archive
-                case *.7z
-                    7z x $archive
-                case '*'
-                    echo "don't know how to extract '$archive'..."
-            end
-        else
-            echo "'$archive' is not a valid file!"
-        end
-    end
-end
+set -Ua fish_user_paths ~/.config/alacritty
 
 # for bash
 # extract() {
