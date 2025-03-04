693fc39f7e73876d428b71a8124ab28135b22243
Ayako <ayako.gfx@gmail.com>
2025-03-04 23:27:20 +0530

    z

---
+++ hyprland/.config/hypr/autorun.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+function run {
+    if ! pgrep $1 > /dev/null ;
+    then
+        $@&
+    fi
+}
+
+run emacs --daemon &
+run nm-applet &
+run waybar &
+run copyq &
+run systemctl --user start hyprpolkitagent
